543bd95a6b6a9f15289901c278de6dc6a74c3422
ErnanLindo <gabriel.ernandes.martins@escola.pr.gov.br>
2026-03-05 11:36:14 -0300

    Create aula4.py

---
+++ aula4.py
@@ -0,0 +1,5 @@
+#int=numeros inteiros EX: 7,0,-4
+#float=numeros reais com virgula com virgula EX: 0,45,7,6
+#bool=valores logicos de verdadeiro ou falso EX:'true ou false'
+#STR=strings que sao palavras caractere ou textos EX: 'ola' , '7.5'
+#Para saber o tipo de uma variavel pode se usar o comando print (type(variavel))
